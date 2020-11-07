63c13abee9530efdb9926828f0f72437c64d2d0d
Drilon Halili <drilon.halili@gmail.com>
2020-11-08 00:20:16 +0100

    final update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
 //Choose a random color to change the background color
 const button = document.querySelector("button");
+const heading = document.querySelector("h1");
 const body = document.querySelector("body");
-const colors = ["blue", "yellow", "pink"];
+const colorName = document.querySelector("#color-name");
+const colors = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "cyan",
+  "blue",
+  "purple",
+  "pink",
+  "white",
+  "gray",
+  "brown",
+];
 
-body.style.backgroundColor = "red";
+body.style.backgroundColor = "";
 button.addEventListener("click", changeBackground);
+colorName.innerHTML = "white".toUpperCase();
 
 function changeBackground() {
   const colorIndex = parseInt(Math.random() * colors.length);
   body.style.backgroundColor = colors[colorIndex];
+  colorName.innerHTML = colors[colorIndex].toUpperCase();
 }
